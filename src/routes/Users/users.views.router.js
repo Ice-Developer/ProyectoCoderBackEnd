5e155c00b0820a8ec1ed490635b1cdac262577cf
JM-Dev <jtogni.developer@gmail.com>
2023-09-07 01:54:40 -0300

    desafio

--- src/routes/Users/users.views.router.js
+++ src/routes/Users/users.views.router.js
@@ -18,12 +18,11 @@ router.get("/", async (req, res) => {
     let page = parseInt(req.query.page);
         if (!page) page = 1;
         let result = await ProductModel.paginate({}, {page, lean: true })
-        let prevLink = result.hasPrevPage ? `http://localhost:8080/profile?page=${result.prevPage}` : '';
-        let nextLink = result.hasNextPage ? `http://localhost:8080/profile?page=${result.nextPage}` : '';
+        let prevLink = result.hasPrevPage ? `http://localhost:8080/users?page=${result.prevPage}` : '';
+        let nextLink = result.hasNextPage ? `http://localhost:8080/users?page=${result.nextPage}` : '';
         let isValid = !(result.page <= 0 || result.page > result.totalPages)
-    res.render('profile', {
-        user: req.session.user,  result, prevLink, nextLink, isValid
-    })
+    
+        res.render('profile', {user: req.session.user,  result, prevLink, nextLink, isValid })
 });
 
 export default router;

68110a045927cb07c6428ed9e46f4892852cff4a
JM-Dev <jtogni.developer@gmail.com>
2023-09-14 13:27:28 -0300

    modifico ruta de db.js

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 const enviorment = async ()=>{
 await mongoose.connect(
     //url de mongoDB
-    "mongodb+srv://jtognidev:00Iceman00@cluster0.hzyqwa1.mongodb.net/ProyectCH",
+    process.env.MONGO_URL,
     {  
         useNewUrlParser: true,
         useUnifiedTopology: true,

20c74b40f3033f98360e3369b10bc6f826bebb8f
JM-Dev <jtogni.developer@gmail.com>
2023-10-03 23:39:38 -0300

    proyecto en capas

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,14 +1,12 @@
 import passport from "passport";
 import envCongif from "./env.config.js";
-/* import passportLocal from "passport-local"; */
 import GitHubStrategy from "passport-github2";
 import jwtStrategy from "passport-jwt";
 import userModel from "../models/userModel.js";
-import { createHash, isValidPassword, PRIVATE_KEY } from "../utils.js";
-/* import UserService from '../../services/user.services.js'; */
+import {PRIVATE_KEY } from "../utils.js";
+
 
 //Declaramos la estrategia a utilizar
-/* const localStrategy = passportLocal.Strategy; */
 const JwtStrategy = jwtStrategy.Strategy;
 const ExtractJwt = jwtStrategy.ExtractJwt;
 
@@ -20,67 +18,14 @@ const initializePassport = () => {
             secretOrKey: PRIVATE_KEY,
         },
         async (jwt_payload, done) => {
-            console.log("Entrando a passport Strategy con JWT");
             try {
-                console.log("JWT obtenido por payload: ", jwt_payload )
                 return done(null, jwt_payload.user);
             } catch (error) {
                 console.error(error);
                 return done(error);
             }
-        }
-    )
-    );
+        }));
 
-    //LocalStrategy     
-    //Register
-/*     passport.use('register', new localStrategy(
-        { passReqToCallback: true, usernameField: 'email'},
-        async (req, res,  username, password, done) => {
-            const { first_name, last_name, email, age} = req.body;
-            try {
-                const exists = await userModel.findOne({ email });
-                if (exists) {
-                    return res.status(400).send({ status: 'error', message: 'usuario ya existe' })
-                };
-                const user = {
-                    first_name,
-                    last_name,
-                    email,
-                    age,
-                    password: createHash(password)
-                }
-                const result = await userModel.create(user);
-                return done(null, result);
-            } catch (error) {
-                return done("Error registrando al usuario" + error);
-            }
-        }
-        )
-    ); */
-
-
-
-    //Login
-/*     passport.use('login', new localStrategy(
-    { passReqToCallback: true, usernameField: 'email'},
-        async (req, username, password, done) => {
-            try {
-                const user = await userModel.findOne({ email:username });
-                if (!user) {
-                    return done(null, false, { message: 'Usuario no encontrado' });
-                };
-                if (!isValidPassword(user, password)) {
-                    return done(null, false, { message: 'Contraseña incorrecta' });
-                };
-                return done(null, user);
-                                
-            } catch (error) {
-                return done("Error logueando al usuario" + error);
-            }
-        }
-    )
-    )  */
 
     //Github Strategy
     passport.use('github',new GitHubStrategy({
@@ -89,10 +34,8 @@ const initializePassport = () => {
         callbackURL: envCongif.gitHubCallbackUrl,
     
     }, async (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
         try {
             const user = await userModel.findOne({ email: profile._json.email});
-            console.log("Usuario encontrado " + user);
             if (!user) {
                 const newUser = {
                     first_name: profile._json.name,
@@ -111,10 +54,7 @@ const initializePassport = () => {
         } catch (error) {
             return done("Error registrando al usuario" + error);
         }
-    
-    }
-    )
-    )
+    }));
 
     //funciones de serializacion y deserializacion
     passport.serializeUser((user, done) => {
@@ -125,7 +65,7 @@ const initializePassport = () => {
             role: user.role,
             };
         done(null, serializedUser);
-    })
+    });
 
     passport.deserializeUser(async (id, done) => {
         try {
@@ -133,10 +73,9 @@ const initializePassport = () => {
             done(null, user);
         } catch (error) {
             console.error("Error deserializando al usuario" + error);
-        }
-    })
+        }});
 
-}
+};
 
 const cookieExtractor=(req)=> {
     let token = null;
@@ -146,6 +85,6 @@ const cookieExtractor=(req)=> {
         console.log("token encontrado " + token);
     }
     return token;
-}
+};
 
 export default initializePassport;
--- src/routes/Mongo/cartRoutes.js
+++ src/routes/Mongo/cartRoutes.js
@@ -1,112 +1,22 @@
 import express from 'express';
-import {CartModel} from '../../models/cartModel.js';
-import { ProductModel } from '../../models/productModel.js';
+import { creatNewCart, searchCart, putProductToCart, deleteProductFromCart, cleanCart } from '../../controllers/cart.controller.js';
 
 const router = express.Router();
 
 //Creamos un carrito
-router.post('/', async (req, res) => {
-    try {
-        const { body } = req;
-        const response = await CartModel.create(body);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(400).json(error.message);
-    }
-});
-
+router.post('/', creatNewCart);
 
 // Buscamos el carrito de compra especifico
-router.get ('/:cid', async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const cart = await CartModel.findOne({ _id : cid}).populate('products.product');
-
-        if (cart) {
-            res.send({ status: 'Success', payload: cart });
-        } else {
-            res.status(404).json({ error: 'Carrito no encontrado' });
-        }
-    } catch (error) {
-        console.error('Error al buscar el carrito:', error);
-        res.status(500).json({ error: 'Error interno del servidor', details: error.message });
-    } 
-});
-
+router.get ('/:cid', searchCart);
 
 //Agregamos un producto especifico al carrito
-router.put('/:cid/products', async (req, res) => {
-    const cid = req.params.cid;
-    const { body } = req;
-    const id = body.id;
-    try {
-        const cart = await CartModel.findOne({ _id: cid });
-        const product = await ProductModel.findOne({ _id: id }); 
-        if (cart && product) {
-            const existingProduct = cart.products.find(p => p.product == id);
-            if (existingProduct) {
-                existingProduct.quantity +=1;
-            } else {
-                cart.products.push({ "product": product.id, "quantity": 1});}// Si el producto no existe en el carrito, lo agregamos
-            await cart.save();// Guardamos los cambios en el carrito
-            res.send({ status: 'Success', payload: cart });
-        } else {
-            res.status(404).json({ error: 'Carrito o producto no encontrado' });
-        }
-    } catch (error) {
-        console.error('Error al buscar el carrito o producto:', error);
-        res.status(500).json({ error: 'Error interno del servidor', details: error.message });
-    }
-});
+router.put('/:cid/products', putProductToCart);
 
-
-//Elimininamos un producto especifico del carrito
-router.delete('/:cid/products/:pid', async (req, res) => {
-    const cid = req.params.cid;
-    const pid = req.params.pid;
-    try {
-        const cart = await CartModel.findOne({ _id: cid });
-        const product = await ProductModel.findOne({ _id: pid }); 
-        
-        if (cart && product) {
-            const existingProduct = cart.products.find(p => p.product == pid);
-            if (existingProduct) {
-                cart.products.splice(p => p.product != pid);
-                
-            }
-            
-            // Guardamos los cambios en el carrito
-            await cart.save();
-            res.send({ status: 'Success', payload: cart });
-        } else {
-            res.status(404).json({ error: 'Carrito o producto no encontrado' });
-        }
-    } catch (error) {
-        console.error('Error al buscar el carrito o producto:', error);
-        res.status(500).json({ error: 'Error interno del servidor', details: error.message });
-    }
-});
-
+//Bajamos la cantidad de un producto especifico al carrito o lo eliminamos en el caso de solo quedar uno
+router.delete('/:cid/products/:pid', deleteProductFromCart);
 
 //limipamos el carrito de compras
-router.put('/:cid', async (req, res) => {
-    const cid = req.params.cid;
-    try {
-        const cart = await CartModel.findOne({ _id: cid });     
-        if (cart) {
-            cart.products.splice (0, cart.products.length);
-            
-            // Guardamos los cambios en el carrito
-            await cart.save();
-            res.send({ status: 'Success', payload: cart });
-        } else {
-            res.status(404).json({ error: 'Carrito o producto no encontrado' });
-        }
-    } catch (error) {
-        console.error('Error al buscar el carrito o producto:', error);
-        res.status(500).json({ error: 'Error interno del servidor', details: error.message });
-    }
-});
+router.put('/:cid/clean', cleanCart);
 
 
 export default router;
